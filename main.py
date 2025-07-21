a06d49a2b869cffb03ed9e736eea814dcc066df3
nicholas-net <cloudvibeclothing@gmail.com>
2025-07-21 12:03:28 -0400

    log BTC price to csv

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 import json
-
 import requests
+import csv
+import datetime as dt
 
 response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
 prices = response.json()
 
 
 for coin, price in prices.items():
-    print(f"BTC price is ${price['usd']:,.2f}")
+    btc_price = price['usd']
+
+# Current time
+time = dt.datetime.now()
+
+
+def log_price(time_stamp: object, price: float) -> None:
+    """
+    Appends the current crypto price to a row in the prices csv file
+
+    Args:
+        time_stamp (str): record of the price at this particular moment in time
+        price (float): how much the crypto coin is being traded for
+
+    Returns:
+        None
+
+    """
+
+    formatted_time = time_stamp.strftime("%G-%m-%dT%H:%M:%S")
+    coin_price = [formatted_time, price]
+
+    with open("prices.csv", "a", newline="") as prices_file:
+        writer = csv.writer(prices_file)
+        writer.writerow(coin_price)
+
+log_price(time, btc_price)
+
+
+
